10ca99235dd03bf54a67d77c64ed9456df16c986
shodaisuzuki <shodai-connect@tbz.t-com.ne.jp>
2018-02-22 17:25:28 +0900

    user#create form create

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   before_save { self.email = email.downcase }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  # validates :name, presence: true,
-  #                  length: { maximum: 50 }
+  validates :name, presence: true,
+                    length: { maximum: 50 }
   validates :email, presence: true,
                     uniqueness: {case_sensitive: false},
                     length: {maximum: 255},
